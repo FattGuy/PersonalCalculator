274e29f462aa75ede79c042d1ede49259d6e1848
FattGuy <fchang@myriadmobile.com>
2018-08-05 08:14:16 -0500

    Initial Commit

---
+++ PersonalCalculatorTests/PersonalCalculatorTests.swift
@@ -0,0 +1,36 @@
+//
+//  PersonalCalculatorTests.swift
+//  PersonalCalculatorTests
+//
+//  Created by Feng Chang on 8/5/18.
+//  Copyright © 2018 Feng Chang. All rights reserved.
+//
+
+import XCTest
+@testable import PersonalCalculator
+
+class PersonalCalculatorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
